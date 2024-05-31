226bb81a3a10a73359c4d7f935b2552578da8129
Achilleas Seisa <achilleas2942@gmail.com>
2024-05-31 02:37:08 +0200

    Initial commit

--- tutorial/run_controller.sh
+++ tutorial/run_controller.sh
@@ -1,2 +1,3 @@
 # Run the docker image for the controller
-docker run --privileged -it --net=host achilleas2942/aerotrain-hri-controller
+xhost +
+docker run --privileged -it --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" achilleas2942/aerotrain-hri-controller:latest
