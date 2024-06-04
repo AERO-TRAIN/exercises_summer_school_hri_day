e2446063b793926b5a35f7c1e5b53fb3aaffab7d
Achilleas Seisa <achilleas2942@gmail.com>
2024-06-04 14:30:42 +0200

    Initial commit

--- tutorial/run_controller.sh
+++ tutorial/run_controller.sh
@@ -1,3 +1,10 @@
-# Run the docker image for the controller
+#!/bin/bash
+
+# Allow connections to the X server
 xhost +
-docker run --privileged -it --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" achilleas2942/aerotrain-hri-controller:latest
+
+# Run the container in detached mode
+container_id=$(docker run --privileged -d --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" achilleas2942/aerotrain-hri-controller:latest)
+
+# Open a bash terminal in the running container for the controller
+docker exec -it $container_id bash
--- tutorial/run_gazebo.sh
+++ tutorial/run_gazebo.sh
@@ -1,3 +1,7 @@
-# Run the docker image for the gazebo simulator
+#!/bin/bash
+
+# Allow connections to the X server
 xhost +
+
+# Run the container for the gazebo simulator
 docker run --privileged -it --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" achilleas2942/aerotrain-hri-gazebo:latest
